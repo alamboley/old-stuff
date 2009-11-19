0bdc8aae62b4e2900fe73f72b8ef0161f5e54729
aymeric.lamboley@gmail.com <aymeric.lamboley@gmail.com@0967230c-3fd2-11de-b83a-779c162a6e82>
2009-11-19 15:08:29 +0000

    Ajout algo + modif main

--- LO44/src/Algorithmes_Genetiques.c
+++ LO44/src/Algorithmes_Genetiques.c
@@ -1,10 +1,10 @@
 /*
  ============================================================================
  Name        : Algorithmes_Genetiques.c
- Author      : Aymeric
+ Author      : Aymeric & Anne
  Version     :
  Copyright   : 
- Description : Hello World in C, Ansi-style
+ Description : Algorithme Génétique
  ============================================================================
  */
 
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("Hello World !"); /* prints Hello World ! */
+	puts("Hello World !");
 	return EXIT_SUCCESS;
 }
