a143fb8b30d08179ab524194671447097be44c7f
alexaugen <alexaugen@0967230c-3fd2-11de-b83a-779c162a6e82>
2010-06-05 15:32:05 +0000

    Remise à jour avec la dernière version qui prend en compte les arétes et les sommets sur la map

--- LO43/Workspace/Project_LO43/src/lo43/projet/utboheme/hexagoneview/SommetView.java
+++ LO43/Workspace/Project_LO43/src/lo43/projet/utboheme/hexagoneview/SommetView.java
@@ -10,27 +10,27 @@ import lo43.projet.utboheme.hexagone.Sommet;
 @SuppressWarnings("serial")
 public class SommetView extends Rectangle{
 	
-	private Sommet s;
-	private Color c;
+	private Sommet sommet;
+	private Color color;
 	
 	public SommetView(Sommet ps, int ptaille){
 		super();
-		this.s = ps;
-		this.setLocation(s.getXSom() - ptaille/2, s.getYSom() - ptaille/2);
+		this.sommet = ps;
+		this.setLocation(sommet.getXSom() - ptaille/2, sommet.getYSom() - ptaille/2);
 		this.setSize(ptaille, ptaille);
-		this.c = Color.orange;
+		this.color = Color.orange;
 	}
 	
 	public void paint(Graphics g){
-		g.setColor(this.c);
-		if(s.hasUV()) {
+		g.setColor(this.color);
+		if(sommet.hasUV()) {
 			g.setColor(Color.blue);
 		}
 		g.fillOval((int)this.getX(), (int)this.getY(), (int)this.getWidth(), (int)this.getHeight());
 	}
 	
 	public void selected(boolean b) {
-		this.c = (b) ? Color.red : Color.orange;
+		this.color = (b) ? Color.red : Color.orange;
 	}
 	
 }
